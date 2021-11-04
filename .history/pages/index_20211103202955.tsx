daa2d24333e9972b0a4db917df6e617dac0d5d49
garcia <josegarcia@unicomfacauca.edu.co>
2021-11-03 21:03:53 -0500

    update

--- .history/pages/index_20211103202955.tsx
+++ .history/pages/index_20211103202955.tsx
@@ -6,7 +6,6 @@ const Home: NextPage = () => {
   return (
     <div className='bg-green-500'>
       <p>Pagina de Index - prueba dev</p>
-      <h1> Hola mi amor te amo muchooooo, ya estoy aprendiendo a desplegar mi página web </h1>
       <Link href='/admin/usuarios'>
         <a className='cursor-pointer'>Ir a admin usuarios</a>
       </Link>
